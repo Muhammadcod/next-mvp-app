3bb16d4f8cc58f5078df953141aeb95e03647d70
mohammedadebayo <adebayomuhammad47@gmail.com>
2025-01-10 22:14:35 +0100

    chore: initiate project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `https://wld-u-rather.netlify.app/:path*`,
+        has: [
+          {
+            type: 'header',
+            key: 'x-should-rewrite',
+            value: 'true',
+          },
+        ],
+      },
+    ]
+  },
+}
 
 export default nextConfig;
